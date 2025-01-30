b83bc2598f0e713587add7256e0204768d2fe114
Boris Dimitrov <borisd2000@gmail.com>
2025-01-30 20:55:18 +0100

    Setup GLFW

--- src/Pekan/main.cpp
+++ src/Pekan/main.cpp
@@ -1,6 +1,33 @@
+#include <GLFW/glfw3.h>
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hey, this is Pekan!" << std::endl;
+    // Initialize GLFW
+    if (!glfwInit())
+    {
+        std::cout << "ERROR: Failed to initialize GLFW." << std::endl;
+        return -1;
+    }
+
+    // Create a window
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Pekan", nullptr, nullptr);
+    if (!window)
+    {
+        std::cout << "ERROR: Failed to create a window with GLFW." << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // Loop until the user closes the window
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
