386568727fc9b7c985cf7433655d70b08129cfc4
Boris Dimitrov <borisd2000@gmail.com>
2025-03-02 20:56:46 +0100

    Separate demos into projects Demo00 and Demo01; Add CMake option WITH_DEMO_PROJECTS for enabling/disabling demo projects

---
+++ src/Demo01/main.cpp
@@ -0,0 +1,18 @@
+#define PK_FILENAME "main.cpp"
+#include "Logger/PekanLogger.h"
+
+#include "Demo01_Application.h"
+using Demo::Demo01_Application;
+
+int main(void)
+{
+    Demo01_Application application;
+    if (!application.init())
+    {
+        PK_LOG_ERRORF("Application failed to initialize.");
+        return -1;
+    }
+    application.run();
+
+    return 0;
+}
